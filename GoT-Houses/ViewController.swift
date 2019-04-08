c2d3050252e0ed93595be1bc7ef5c2f20ab623eb
Cam Arrigo <carrigo3@gmail.com>
2019-04-08 16:39:52 -0400

    Initial Commit

---
+++ GoT-Houses/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoT-Houses
+//
+//  Created by Cameron Arrigo on 4/8/19.
+//  Copyright © 2019 Cameron Arrigo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
